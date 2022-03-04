ebc11014cf25e43c5d12a6c4f89304ab2a2c3827
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-03-04 03:28:42 +0000

    added code for python unit2 day 3

---
+++ Constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Ferdous Sediqi
+# Created on: March 3rd, 2022
+# This program asks the user for the radius of
+# a circle in mm. It then calculates and displays
+# the circumference using tau.
+
+
+TAU = 6.28
